d53bf9440a030741bb579c4e5ab9d03d87caaec4
Marcos <marcos.paulo.mpsb99@gmail.com>
2024-11-28 19:18:36 -0300

    feature:configurando segurança

---
+++ src/main/java/br/com/mural/criando/futuro/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package br.com.mural.criando.futuro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Criando um usuário em memória com credenciais simples
+        var user = User.withUsername("teste")
+                .password(passwordEncoder().encode("teste"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user); // Gerenciador de usuários em memória
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/css/**", "/js/**").permitAll()
+                                .requestMatchers("/dashboard").authenticated()
+                                .anyRequest().permitAll()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/dashboard", true)
+                                .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
+                );
+
+        return http.build();
+    }
+}
--- src/main/java/br/com/mural/criando/futuro/service/ImgurService.java
+++ src/main/java/br/com/mural/criando/futuro/service/ImgurService.java
@@ -12,36 +12,25 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImgurService {
 
-    private static final String IMGUR_CLIENT_ID = "151c60fcb07a72e"; // Substitua pelo seu Client ID
+    private static final String IMGUR_CLIENT_ID = "151c60fcb07a72e";
 
     public String uploadImage(MultipartFile file) throws Exception {
-        // Crie a URL da API do Imgur
         String url = "https://api.imgur.com/3/image";
 
-        // Crie um cabeçalho para a requisição
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Client-ID " + IMGUR_CLIENT_ID);
 
-        // Prepare o arquivo para envio\
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         body.add("image", file.getBytes());
 
-        // Crie a requisição com o cabeçalho e o arquivo
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-        // Use RestTemplate para enviar a requisição POST para o Imgur
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
 
-        // Analise a resposta para obter a URL da imagem
         if (response.getStatusCode() == HttpStatus.OK) {
-            // Use o ObjectMapper para parsear o JSON
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode jsonResponse = objectMapper.readTree(response.getBody());
-
-            // Acesse o campo "link" dentro de "data"
-
-            // Retorna o link da imagem
             return jsonResponse.path("data").path("link").asText();
         }  else {
             throw new Exception("Erro no upload da imagem.");
